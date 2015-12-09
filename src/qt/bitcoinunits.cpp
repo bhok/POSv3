102ae5e94569bf7870ed45f838ec0f463f62beca
pinkmagicdev <pinkmagicdev@theblockchain.me>
2015-12-09 05:25:18 +0100

    start

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BLK");
-    case mBTC: return QString("mBLK");
-    case uBTC: return QString::fromUtf8("μBLK");
+    case BTC: return QString("BUCKS");
+    case mBTC: return QString("mBUCKS");
+    case uBTC: return QString::fromUtf8("μBUCKS");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BlackCoins");
-    case mBTC: return QString("Milli-BlackCoins (1 / 1,000)");
-    case uBTC: return QString("Micro-BlackCoins (1 / 1,000,000)");
+    case BTC: return QString("SwagBuckss");
+    case mBTC: return QString("Milli-SwagBuckss (1 / 1,000)");
+    case uBTC: return QString("Micro-SwagBuckss (1 / 1,000,000)");
     default: return QString("???");
     }
 }
